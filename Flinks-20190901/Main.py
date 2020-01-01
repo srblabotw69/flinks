943b62c687ea06b90dd952d90f882a4b7549042c
srblabotw69 <mus56789@gmail.com>
2019-12-31 22:01:30 -0500

    Add files via upload
    This is the original code to access flinks api Sept 2019.

---
+++ Flinks-20190901/Main.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template
+from flask import request
+
+from requests import post
+import json
+
+DEBUG = True
+app = Flask(__name__)
+
+
+@app.route('/enkidu', methods=('GET', 'POST'))
+def enkidu():
+    return render_template('enkidu.html')
+
+
+@app.route('/flinks', methods=['POST'])
+def flinks():
+    # POST request
+    if request.method == 'POST':
+        print('Incoming...')
+        print(request.get_json())  # parse as JSON
+
+        headers = {'Content-Type': 'application/json'}
+
+        # parse loginID:
+        y = request.get_json()
+        # print(y["LoginId"])
+        lid = y["LoginId"]
+        sdata = {"LoginId": lid, "MostRecentCached": True}
+
+        # convert into JSON:
+        data = json.dumps(sdata)
+
+        try:
+            response = post(
+                "https://toolbox-api.private.fin.ag/v3/43387ca6-0391-4c82-857d-70d95f087ecb/BankingServices/Authorize",
+                headers=headers, data=data).json()
+            # print(response)
+            print(response['RequestId'])
+
+            rid = response['RequestId']
+            tdata = {"RequestId": rid}
+
+            # convert into JSON:
+            data = json.dumps(tdata)
+            print(data)
+
+            response = post(
+                "https://toolbox-api.private.fin.ag/v3/43387ca6-0391-4c82-857d-70d95f087ecb/BankingServices/GetAccountsDetail",
+                headers=headers, data=data).json()
+            print(response)
+
+            # print(json.dumps(response, sort_keys=True,
+            #        indent=4, separators=(',', ': ')))
+
+            # print(response["Accounts"])
+
+            # m = json.loads(tdata)
+            # print(m)
+            # n = json.loads(m)
+            #
+            #
+            array_length = len(response)
+            # print(array_length)
+            for i in range(array_length):
+                print(" ")
+                print("Title: " + response["Accounts"][i]["Title"]),
+                print("Balance: " + json.dumps(response["Accounts"][i]["Balance"])),
+                print("Category: " + response["Accounts"][i]["Category"]),
+                print("Type: " + response["Accounts"][i]["Type"]),
+                print("Currency: " + response["Accounts"][i]["Currency"])
+                print("==================================")
+
+        except Exception as e:
+            print(e)
+
+    return 'OKie', 200
+
+    # # GET request
+    # else:
+    #     message = {'greeting':'Hello from Flask!'}
+    #     return jsonify(message)  # serialize and use JSON headers
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# {'Links': [{'rel': 'AccountsDetail', 'href': '/GetAccountsDetail', 'example': None},
+#            {'rel': 'AccountsSummary', 'href': '/GetAccountsSummary', 'example': None},
+#            {'rel': 'Statements', 'href': '/GetStatements', 'example': None}],
+#  'HttpStatusCode': 200,
+#  'Login': {'Username': 'Greatday', 'IsScheduledRefresh': False, 'LastRefresh': '2019-10-04T09:08:07.8216522',
+#            'Type': 'Personal', 'Id': 'a5c1923b-c61a-4783-eca4-08d74861abc3'},
+#  'Institution': 'FlinksCapital', 'RequestId': '27ad4fb0-1441-41e4-82ce-54ef1f2896cc'}
